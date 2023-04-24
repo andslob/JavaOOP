032112302e05081ee402d27b9bbc2c6c4267b190
Andrey Slobodchikov <andslob@inbox.ru>
2023-04-24 12:41:45 +0500

    hw2

---
+++ HomeWork/hw2/Plate.java
@@ -0,0 +1,23 @@
+package JavaOOP.HomeWork.hw2;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void info() {
+        System.out.printf("plate: %d\n\n",food);
+    }
+    public int getFood() {
+        return food;
+    }
+    public void setFood(int food) {
+        this.food = food;
+    }
+    public void addFood(int add) {
+        this.food += add;
+        System.out.printf("!!!Add %d foods\n\n", add);
+    }
+}
+
