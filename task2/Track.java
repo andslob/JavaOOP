c18f1a2c01102401059c5d92e570a80bdb88d735
Andrey Slobodchikov <andslob@inbox.ru>
2023-04-24 11:34:27 +0500

    task2

---
+++ task2/Track.java
@@ -0,0 +1,20 @@
+package JavaOOP.task2;
+
+public class Track implements Obstacle {
+
+    private int length;
+
+    public Track(int length){
+        this.length = length;
+    }
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+}
